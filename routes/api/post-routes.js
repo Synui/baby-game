bc3f68980a9bdfcf9ef356696b8818baa1117e5f
Kenia Lizama-Gamez <ke.yulizama@gmail.com>
2022-02-14 13:41:07 -0500

    single post

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -21,4 +21,41 @@ router.get('/', (req, res) => {
     });
 });
 
+
+// get a singe post
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'title', 'created_at'],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// create a post
+router.post('/', (req, res) => {
+    Post.create({
+        title: req.body.title,
+        user_id: req.body.user_id
+    })
+    
+})
+
 module.exports = router;
