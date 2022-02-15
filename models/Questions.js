93e285e9777972147752ec95c46d4da7c2f5c609
Kenia Lizama-Gamez <ke.yulizama@gmail.com>
2022-02-15 13:18:58 -0500

    questions

---
+++ models/Questions.js
@@ -0,0 +1,24 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Vote extends Model {}
+
+Vote.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        // 
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'vote'
+    }
+);
+
+module.exports = Vote;
